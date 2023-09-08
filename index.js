f45eda413aa4c9927354fd9e4be4e4178fb31853
saraphina1 <oyeyemibunmi21@gmail.com>
2023-09-08 23:26:25 +0100

    coverted to milliseconds

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 const dateElement = document.querySelector(".date");
 const timeElement = document.querySelector(".time");
 
+const date= new Date();
 
+const DAYS=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const dayOfTheWeek= DAYS[date.getDay()];
+dateElement.textContent=dayOfTheWeek;
 
-function formatDate(date){
-    const DAYS=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const MONTHS=["January","February","March","April","May","June","July","August","September","October","November","December"]
-
-    return `${DAYS[date.getDay()]}`
-}
-function formatTime(date){
-    const hours12= date.getHours() % 12 || 12;
-    const minutes= date.getMinutes();
-    const isAM = date.getHours() < 12;
-
-    return `${hours12.toString().padStart(2, "0")}: ${minutes.toString().padStart(2, "0")} ${isAM ? "AM" : "PM"}`;
+function currentMillisecond(){
+    const now= new Date();
+    const todayMilliseconds= Date.now();
+    timeElement.textContent= todayMilliseconds;
 }
 
-setInterval(()=>{
-    const current= new Date();
-    dateElement.textContent= formatDate(current);
-    timeElement.textContent=formatTime(current);
-}, 200);
+setInterval(currentMillisecond, 1000);
+
+
+
+// function formatDate(date){
+//     const DAYS=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+//     return `${DAYS[date.getDay()]}`
+// }
+// //  function formatTime(date){
+// //          const hours12= date.getHours() % 12 || 12;
+// //     const minutes= date.getMinutes();
+// //      const isAM = date.getHours() < 12;
+
+// //      return `${hours12.toString().padStart(2, "0")}: ${minutes.toString().padStart(2, "0")} ${isAM ? "AM" : "PM"}`;
+// //  }
+
+//  function formatTime(){
+
+// let timee= Date.now()
+// return timee
+// }
+// setInterval(()=>{
+//     const current= new Date();
+    
+//     dateElement.textContent= formatDate(current);
+//     timeElement.textContent=formatTime(current);
+// },1000);
